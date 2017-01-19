cc419474186bb7b8ef992d37330e2917dd94e022
menshiyun <men_shiyun@qq.com>
2017-01-19 12:00:41 +0800

    修改child_thread

--- child_thread.c
+++ child_thread.c
@@ -1,46 +1,45 @@
-#include <stdio.h>
-#include <sched.h>
 #include <pthread.h>
+#include <sched.h>
+#include <stdio.h>
 
-void *child_thread(void *arg)
-{
+void *child_thread(void *arg) {
   int policy;
-  int max_priority,min_priority;
+  int max_priority, min_priority;
   struct sched_param param;
   pthread_attr_t attr;
 
   pthread_attr_init(&attr);
-  pthread_attr_setinheritsched(&attr,PTHREAD_EXPLICIT_SCHED);
 
-  pthread_attr_getinheritsched(&attr,&policy);
-  if(policy==PTHREAD_EXPLICIT_SCHED)
+  pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+  pthread_attr_getinheritsched(&attr, &policy);
+  if (policy == PTHREAD_EXPLICIT_SCHED)
     printf("Inheritsched:PTHREAD_EXPLICIT_SCHED\n");
-  if(policy==PTHREAD_INHERIT_SCHED)
+  if (policy == PTHREAD_INHERIT_SCHED)
     printf("Inheritsched:PTHREAD_INHERIT_SCHED\n");
 
-  pthread_attr_setschedpolicy(&attr,SCHED_RR);
-  pthread_attr_getschedpolicy(&attr,&policy);
-  if(policy==SCHED_FIFO)
+  pthread_attr_setschedpolicy(&attr, SCHED_RR);
+  pthread_attr_getschedpolicy(&attr, &policy);
+  if (policy == SCHED_FIFO)
     printf("Schedpolicy:SCHED_FIFO\n");
-  if (policy==SCHED_RR)
+  if (policy == SCHED_RR)
     printf("Schedpolicy:SCHED_RR\n");
-  if(policy==SCHED_OTHER)
+  if (policy == SCHED_OTHER)
     printf("Schedpolicy:SCHED_OTHER\n");
 
   sched_get_priority_max(max_priority);
   sched_get_priority_min(min_priority);
-  printf("Max priority:%u\n",max_priority);
-  printf("Min priority:%u\n",min_priority);
+  printf("Max priority:%u\n", max_priority);
+  printf("Min priority:%u\n", min_priority);
 
-  param.sched_priority=max_priority;
-  pthread_attr_setschedparam(&attr,&param);
-  printf("sched_priority:%u\n",param.sched_priority);
+  param.sched_priority = max_priority;
+  pthread_attr_setschedparam(&attr, &param);
+  printf("sched_priority:%u\n", param.sched_priority);
   pthread_attr_destroy(&attr);
 }
 
 int main(int argc, char const *argv[]) {
   pthread_t child_thread_id;
-  pthread_create(&child_thread_id,NULL,child_thread,NULL);
-  pthread_join(child_thread_id,NULL);
+  pthread_create(&child_thread_id, NULL, child_thread, NULL);
+  pthread_join(child_thread_id, NULL);
   return 0;
 }
